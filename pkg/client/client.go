32704824f2e728881faf648d6f00c924c510f583
Victor Palade <palade.ionut@gmail.com>
2017-01-03 00:35:27 +0100

    implement a working minio client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,16 +4,43 @@ import (
 	minio "github.com/minio/minio-go"
 )
 
+const (
+	location = "us-east-1"
+)
+
+// MinioClient is a wrapper around minio.Client that also holds the bucket name
+// where we want to copy files.
 type MinioClient struct {
-	Client     *minio.Client
-	BucketInfo *minio.BucketInfo
+	Client          *minio.Client
+	BucketName      string
+	ServerURI       string
+	AccesKeyID      string
+	SecretAccessKey string
 }
 
-// NewMinioClient returns a new minio client based on passed access specs.
-func NewMinioClient(serverURI, accessKeyID, secretAccessKey string, secure bool) (*minio.Client, error) {
+// NewMinioClient returns a new minio client based on passed access specs and
+// creates a new bucket if it doesn't exist.
+func NewMinioClient(serverURI, accessKeyID, secretAccessKey, bucket string, secure bool) (*MinioClient, error) {
 	c, err := minio.New(serverURI, accessKeyID, secretAccessKey, secure)
 	if err != nil {
 		return nil, err
 	}
-	return c, nil
+
+	exists, err := c.BucketExists(bucket)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		if err := c.MakeBucket(bucket, location); err != nil {
+			return nil, err
+		}
+	}
+
+	return &MinioClient{
+		Client:          c,
+		BucketName:      bucket,
+		ServerURI:       serverURI,
+		AccesKeyID:      accessKeyID,
+		SecretAccessKey: secretAccessKey,
+	}, nil
 }
