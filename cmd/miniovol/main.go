86f503d0109767261ec32fb2006312a3b3c2110b
Victor Palade <palade.ionut@gmail.com>
2017-02-03 19:00:48 +0100

    shorten var

--- cmd/miniovol/main.go
+++ cmd/miniovol/main.go
@@ -20,16 +20,16 @@ const (
 // Error implements the error interface and uses it to return a generic
 // validation error for environment variables that are mandatory.
 type Error struct {
-	envVariable string
+	envVar string
 }
 
 func (e Error) Error() string {
-	return fmt.Sprintf("environment variable %s cannot be empty", e.envVariable)
+	return fmt.Sprintf("environment variable %s cannot be empty", e.envVar)
 }
 
 func newErrEmptyEnvVar(v string) error {
 	return Error{
-		envVariable: v,
+		envVar: v,
 	}
 }
 
@@ -43,18 +43,18 @@ func getEnvDetails() (serverURI, accessKeyID, secretAccessKey, bucket string, se
 		err = newErrEmptyEnvVar("MINIO_SERVER")
 		return
 	}
-
 	accessKeyID = os.Getenv("MINIO_ACCESSKEY")
 	if accessKeyID == "" {
 		err = newErrEmptyEnvVar("MINIO_ACCESSKEY")
 		return
 	}
-
 	secretAccessKey = os.Getenv("MINIO_SECRETKEY")
 	if secretAccessKey == "" {
 		err = newErrEmptyEnvVar("MINIO_SECRETKEY")
 		return
 	}
+	// bucket can be empty, since we're gonna generate a new bucket name if there
+	// wasn't one specified.
 	bucket = os.Getenv("MINIO_BUCKET")
 
 	return
